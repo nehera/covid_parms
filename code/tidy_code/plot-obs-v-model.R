aa21855e7b9844810dd66d0cf602f4f9a6cbc46c
nehera <atm.neher@gmail.com>
2021-02-26 11:03:47 -0600

    parallel crawl factor experiments

--- code/tidy_code/plot-obs-v-model.R
+++ code/tidy_code/plot-obs-v-model.R
@@ -1,9 +1,9 @@
 set.seed(12995)
 
-state.mid <- c("MN", "WI", "ND", "MI", "OH", "IN", "IL", "IA", "MO", "SD", "NE", "KS")
+state.mid <- c("MN", "WI", "ND", "OH", "IN", "IL", "IA", "MO", "SD", "KS") # "MI", "NE"
 
 # define variables
-run <- "2021-02-20 13-56-28"
+run <- "2021-02-25 19-21-09"
 run_dir <- paste("~/Desktop/covid_parms/data/tidy_data/runs/", run, sep = "")
 nsim <- 10000 # number of simulations
 dsim <- 228 # days to simulate
@@ -24,7 +24,7 @@ state_positives <- read_csv("state-positives.csv")
 setwd("~/Desktop/covid_parms/figures/exp_figures/")
 pdf("state_obs_v_pred.pdf")
 
-for (j in 1:3) { 
+for (j in 1:10) { 
   # pull state-specific data
   state_of_interest <- as.character(state.mid[j])
   phases <- subset(state_phases, State==state_of_interest)
@@ -38,44 +38,6 @@ for (j in 1:3) {
   setwd(run_dir)
   pred <- read_rds(paste(state_of_interest, "_seir.rds", sep = ""))
   rmse <- read_rds(paste(state_of_interest, "_rmse.rds", sep = ""))
-  
-  ###
-  # state_rmse <- data.frame(matrix(nrow = 0, ncol = 3))
-  # colnames(state_rmse) <- c("sim_id", "type", "value")
-  # 
-  # for (i in rmse$sim_id) { 
-  #   rmse_temp <- data.frame(matrix(nrow = 0, ncol = 3))
-  #   colnames(rmse_temp) <- c("sim_id", "type", "value")
-  #   
-  #   m_pos_init <- pred %>% subset(sim_id == i)
-  #   m_pos <- cumsum(m_pos_init$Is * pop)
-  # 
-  #   p_start <- 1
-  #   for (k in 1:phase_num) {
-  #     if (k==phase_num) {
-  #       p_end <- dsim
-  #     } else {
-  #       p_end <- as.numeric(phases[k+2])
-  #     }
-  #     
-  #     days <- p_start:p_end
-  #     len <- length(days)
-  # 
-  #     rmse_ph <- sqrt(RMSE(m_pos[days], o_pos[days])^2*len*(1-len/dsim)) # transforms traditional rmse into weighted rmse proposed here https://stats.stackexchange.com/questions/230517/weighted-root-mean-square-error
-  #     rmse_temp <- rbind(rmse_temp, data.frame(sim_id=i, type=paste("p",k,sep = ""), value=rmse_ph))
-  #     
-  #     if (k!=phase_num) {
-  #       p_start <- p_end+1
-  #     }
-  #   }
-  #   
-  #   rmse_ag <- (sum(rmse_temp$value))/phase_num
-  #   rmse_temp <- rbind(rmse_temp, data.frame(sim_id=i, type="ag", value=rmse_ag))
-  #   state_rmse <- rbind(state_rmse, rmse_temp)
-  # }
-  ###
-  
-  # rmse_filt <- subset(state_rmse, type == "ag")
   
   up_r <- quantile(rmse$value, upper)
   sim_acc <- subset(rmse, value <= up_r)$sim_id
