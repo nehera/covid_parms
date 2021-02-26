c03654a8fba8459757ee7ebd20abe1e6dc0b539f
nehera <atm.neher@gmail.com>
2021-02-26 16:16:29 -0600

    State time to Dec 11, 2020
    On deciding to terminate our simulations at the point at which vaccines were approved for emergency use, we're modifying the model to terminate simulations at Dec 11, 2020

---
+++ code/tidy_code/pull-day-ranges.R
@@ -0,0 +1,20 @@
+end <- ymd("2020-12-11") # Vaccines approved for emergency use by FDA 
+
+library(plyr)
+library(lubridate)
+
+setwd("~/Desktop/covid_parms/data/tidy_data")
+state_positives <- read.csv("state-positives.csv")
+
+states <- as.list(state.abb)
+
+df <- as.data.frame(matrix(nrow = 50, ncol = 3))
+colnames(df) <- c("state", "d1", "diff")
+
+extract.dates <- function(state_of_interest) {
+  d1 <- ymd(state_positives[which(state_positives$state==state_of_interest),1][1])
+  diff <- as.numeric(end-d1)
+  return(data.frame(state=state_of_interest,d1=d1,diff=diff))
+}
+
+res <- ldply(lapply(states, extract.dates),rbind)
