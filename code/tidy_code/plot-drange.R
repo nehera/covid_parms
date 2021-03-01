220c87d5b2601f9b2216da80783b99fc71707d2e
nehera <atm.neher@gmail.com>
2021-03-01 15:00:58 -0600

    Plot midwest day ranges + inc recalc midwest phases

--- code/tidy_code/plot-drange.R
+++ code/tidy_code/plot-drange.R
@@ -1,6 +1,10 @@
+set.seed(12995)
+
 library(data.table)
-library(dplyr)
+library(plyr)
+library(lubridate)
 library(ggplot2)
+library(forcats)
 
 setwd("~/Desktop/covid_parms/data/tidy_data")
 state_phases <- fread("state-phases.csv") # would be nice to denote state phase breakpoints on plot
@@ -10,33 +14,31 @@ state_dsim <- fread("state-time-to-dec11.csv") # need to imp dsims thru dec 11
 state.mid <- as.list(c("MN", "WI", "ND", "MI", "OH", "IN", "IL", "IA", "MO", "SD", "NE", "KS")) # need to make Midwest specific
 
 df <- data.frame()
-for (i in state.abb) {
+for (i in state.mid) {
   state <- subset(state_positives, state==i)
-  d_max <- nrow(state)
   sim_start <- state$date[1]
-  sim_end <- state$date[dsim]
-  df <- rbind(df, data.frame(state=i,d_max=d_max,sim_start=sim_start,sim_end=sim_end))
+  sim_end <- "2020-12-11"
+  phases <- subset(state_phases, State==i)[,3:5]
+  df <- rbind(df, data.frame(id=i,min=sim_start,max=sim_end,p2_0=phases$Phase2_0,p3_0=phases$Phase3_0,p4_0=phases$Phase4_0))
 }
-# dsim_min <- subset(df, d_max == min(df$d_max))
-# WV_date <- state_positives %>%
-#   subset(state=="WV")
-# WV_date[228,]  
-# dsim_max <- subset(df, d_max == max(df$d_max))
-# WA_date <- state_positives %>%
-#   subset(state=="WA")
-# WA_date[228,]  
 
-df2 <- data.frame(id=df$state, min=df$sim_start, max=df$sim_end)
+df$min <- ymd(as.POSIXct(df$min))
+df$max <- ymd(as.POSIXct(df$max))
 
-library(forcats)
+df$p2_0 <- df$min+df$p2_0
+df$p3_0 <- df$min+df$p3_0
+df$p4_0 <- df$min+df$p4_0
 
 setwd("~/Desktop/covid_parms/figures/tidy_figures")
 pdf(file="state-day-ranges.pdf")
 
-ggplot(df2, aes(x=fct_rev(id))) +
-  geom_linerange(aes(ymin=min,ymax=max),linetype=2,color="blue")+
-  geom_point(aes(y=min),size=0.5,color="red") +
-  geom_point(aes(y=max),size=0.5,color="red") +
+ggplot(df, aes(x=fct_rev(id))) +
+  geom_linerange(aes(ymin=min,ymax=max),linetype=2,color="black")+
+  geom_point(aes(y=min),size=1,color="red") +
+  geom_point(aes(y=max),size=1,color="red") +
+  geom_point(aes(y=p2_0),size=1,color="red") +
+  geom_point(aes(y=p3_0),size=1,color="red") +
+  geom_point(aes(y=p4_0),size=1,color="red") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
   xlab("State") + ylab("Date") +
